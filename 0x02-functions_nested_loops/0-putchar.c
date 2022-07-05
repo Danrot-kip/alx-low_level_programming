836c9cd61b7790e2ca8a8963f2caf0e8bcaa91c8
Kipyegon Rotich <danyego100@gmail.com>
2022-07-05 12:31:02 -0700

    test

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * putchar - writes the character c to stdout
+ * @_putchar: The character to print
  *
  * Return: on succes 1.
  * on error , -1 is returned, and errno is set appropriately
