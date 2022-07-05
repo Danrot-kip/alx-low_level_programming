51dde2ea95ddbff31039e98944e0ca607b2cfb1f
Kipyegon Rotich <danyego100@gmail.com>
2022-07-05 12:28:33 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,8 +7,8 @@
  * Return: on succes 1.
  * on error , -1 is returned, and errno is set appropriately
  */
-int _putchar(char c)
+int putchar(char _putchar)
 {
-	return (write(1, &c, 1));
+	return (write(1, &_putchar, 1));
 }
 
