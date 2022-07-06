a39f6bdafac428e3821b956cc35aa18b4c5780e8
Kipyegon Rotich <danyego100@gmail.com>
2022-07-06 16:55:49 -0700

    test

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * _abs - _abs prints out the absolute value
+ *@int: the unteger we are checking
+ * Return: return the absolute value
+ */
+int _abs(int)
+{
+	int r, n;
+
+	r = abs(n);
+	_puthar(n);
+}
