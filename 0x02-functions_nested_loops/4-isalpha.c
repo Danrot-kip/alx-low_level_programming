ad88f7fe53412ffe0bd6906523ec8641610e3efd
Kipyegon Rotich <danyego100@gmail.com>
2022-07-06 15:29:14 -0700

    nested if

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * _isalpha - checks c if lower or upper
+ * @c: c is lower or upper case
+ *
+ * Return: 0 if success else otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		if (c <= 'z' && c >= 'a')
+		{
+			return (1);
+		}
+		return (1);
+	}
+	return (0);
+}
