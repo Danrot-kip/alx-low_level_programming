6db3e93ffd6d3126bcf98d8283a917bd6dd9252c
Kipyegon Rotich <danyego100@gmail.com>
2022-07-06 09:02:30 -0700

    using islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - checks c if it is small letter
+ *
+ * @c:check if it is lower case
+ * Return: 0 otherwise and return 1 if success
+ */
+int _islower(int c)
+{
+	int letter = 'c';
+
+	if (islower(letter))
+	{
+		_putchar('0');
+		_putchar('\n');
+	}
+	else
+
+	return (0);
+}
