efdbe9aa4e56d314b3f2f5f2a94eb9953c03a71c
Kipyegon Rotich <danyego100@gmail.com>
2022-07-06 17:54:46 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ *print_last_digit - prnts the last digit
+ *@n: integer to pe printe
+ *Return: return the last digit
+ */
+
+int print_last_digit(int n)
+{
+	_putchar (n % 10 + '0');
+	return (0);
+}
