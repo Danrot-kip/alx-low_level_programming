60a32b912e2ed9ff2107da018d70afe1e41e198c
Kipyegon Rotich <danyego100@gmail.com>
2022-07-07 13:35:56 -0700

    checks positive - negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+
+/**
+ * main - test 0 if it is positive or negative
+ * Return: 0 if it us true
+ */
+
+
+
+int main(void)
+
+{
+
+	int i;
+
+
+
+	i = 0;
+
+
+
+	positive_or_negative(i);
+
+	return (0);
+
+}
