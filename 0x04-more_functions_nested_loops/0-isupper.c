a68ef379bdaef15280ec2cff3797d25ae7a10cf8
Kipyegon Rotich <danyego100@gmail.com>
2022-07-08 11:19:37 -0700

    test

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - main function
+ * @c: upper case
+ * Return: 1 sucees and 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	for (c >= 'A' && C <= 'Z')
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+	return (0);
+}
