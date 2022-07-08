9afba8af44056f72ccde2fa1600e1de29cedd974
Kipyegon Rotich <danyego100@gmail.com>
2022-07-08 14:28:56 -0700

    print 0-14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - printing more numbers
+ *
+ * return: Return numbers 0to 14
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			_putchar((y / 10) + '0');
+		}
+		_putchar((y % 10) + '0');
+	}
+	_putchar('\n');
+}
