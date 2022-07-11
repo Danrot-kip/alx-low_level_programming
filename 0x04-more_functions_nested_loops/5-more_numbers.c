0360ca7acfafa15322c0c7dbe707d7cc95025bbf
Kipyegon Rotich <danyego100@gmail.com>
2022-07-11 10:15:37 -0700

    nu,bers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,8 +15,11 @@ void more_numbers(void)
 		for (y = 0; y <= 14; y++)
 		{
 			if (y > 9)
+			{
 				_putchar('0' + y / 10);
-			_putchar('0' + y % 10);
+				_putchar('0' + y % 10);
+			}
 		}
-	_putchar('\n');
+		_putchar('\n');
+	}
 }
