b858ea97681b1392f2746d537f22a853279a3109
Kipyegon Rotich <danyego100@gmail.com>
2022-07-11 11:08:03 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle
+ * @s: parameter
+ * Return: null
+ */
+
+void print_triangle(int s)
+{int x, y, k;
+
+if (s > 0)
+{
+for (x = 0; x < s ; x++)
+{
+for (y = s - x ; y > 1 ; y--)
+_putchar(32);
+for (z = 0 ; z <= x ; z++)
+_putchar (35);
+_putchar('\n');
+}
+}
+else
+_putchar ('\n');
+}
+
