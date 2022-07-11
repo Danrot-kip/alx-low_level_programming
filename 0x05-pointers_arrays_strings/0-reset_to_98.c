9102fe219fbcda114bb2184da9b012f09de6dbc5
Kipyegon Rotich <danyego100@gmail.com>
2022-07-11 07:59:51 -0700

    return 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes the value in the memory
+ *
+ * Return: void or null
+ */
+
+
+
+void reset_to_98(int *n)
+{
+	*n=98;
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+int _putchar(char c);
 void _puts(char *str);
 int _strlen(char *s);
 void reset_to_98(int *n);
