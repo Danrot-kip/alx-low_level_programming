7f36ec767efeca3dde4a4f588c8d41253d9ec4a5
Kipyegon Rotich <danyego100@gmail.com>
2022-07-18 05:15:36 -0700

    pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strstr -  main descripttion
+ * @haystack: string where to search
+ * @needle: string to search
+ * Return: char * string
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+if (haystack == NULL || needle == NULL)
+return (NULL);
+
+if (*needle == '\0')
+return (haystack);
+
+while (*haystack != '\0')
+{
+if (*haystack == needle[0])
+{
+if (compara(haystack, needle))
+return (haystack);
+}
+haystack++;
+}
+
+return (NULL);
+}
