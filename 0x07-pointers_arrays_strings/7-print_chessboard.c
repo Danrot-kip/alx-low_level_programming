7a31f080f8f75abd1fa92098d7473b519ded1376
Kipyegon Rotich <danyego100@gmail.com>
2022-07-21 02:47:48 -0700

    Functions

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,19 +1,31 @@
 #include "main.h"
+#include<stdio.h>
 /**
- * print_chessboard - function that prints the chessboard.
- * @a: matrix 8*8
- * Description:  function that prints the chessboard.
- * Return: void
+ * print_chessboard - function that prints the chessboard
+ * @a: Pointer to 2-D array location
+ * Return: Null
  */
+
 void print_chessboard(char (*a)[8])
+
 {
-int file, columns;
-for (file = 0 ; file < 8 ; file++)
-{
-for (columns = 0 ; columns < 8; columns++)
-{
-_putchar(a[file][columns]);
-}
-_putchar('\n');
-}
+
+	int i;
+
+	int j;
+
+
+
+	for (i = 0; i < 8; i++)
+
+	{
+
+		for (j = 0; j < 8; j++)
+
+			putchar(a[i][j]);
+
+	putchar('\n');
+
+	}
+
 }
