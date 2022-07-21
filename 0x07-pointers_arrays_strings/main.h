2d10b494d58ec97207821c0c351587ccb96d5954
Kipyegon Rotich <danyego100@gmail.com>
2022-07-21 02:45:57 -0700

    pointers

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include<stdio.h>
 char *_memset(char *s, char b, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
