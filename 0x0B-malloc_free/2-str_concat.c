ce70faede42e5b8415b7aa106eeac5007ce08aaf
Kipyegon Rotich <danyego100@gmail.com>
2022-07-26 14:28:34 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,43 +1,36 @@
 #include "main.h"
-#include <stdlib.h>
+
 /**
- * str_concat - concatenates two strings.
- * @s1: string 1
- * @s2: string 2
- * Description:  concatenates two strings..
- * Return: pointer to char
+ * str_concat - concatenate two string
+ *
+ * @s1: string
+ * @s2: string
+ *
+ * Return: pointer to newly allocated space or NULL in failure
  */
+
 char *str_concat(char *s1, char *s2)
 {
-	char *ptrStr;
-	int  i, lenCad = 0;
+	char *new_str;
+	int i, len, _len;
 
-	if (s1 ==  NULL && s1 ==  NULL)
-	{
-		ptrStr = "";
-		return (ptrStr);
-	}
-	if (s1 !=  NULL)
-	{
-		for (i = 0; s1[i] != '\0' ; i++)
-			lenCad++;
-	}
-	if (s2 !=  NULL)
-	{
-		for (i = 0; s2[i] != '\0' ; i++)
-			lenCad++;
-	}
-	ptrStr =  malloc((lenCad + 1) * sizeof(char));
-	if (ptrStr == NULL)
+	if (!s1)
+		s1 = "";
+
+	if (!s2)
+		s2 = "";
+
+	len = strlen(s1);
+	_len = strlen(s2);
+
+	new_str = malloc(sizeof(char) * (len + _len + 1));
+
+	if (!new_str)
 		return (NULL);
-	for (i = 0; s1[i] != '\0' ; i++)
-	{
-		ptrStr[i] = s1[i];
-	}
-	for (i = 0; s2[i] != '\0' ; i++)
-	{
-		ptrStr[i] = s2[i];
-	}
-	ptrStr[i] = '\0';
-	return (ptrStr);
+
+	for (i = 0; i <= len + _len; i++)
+		new_str[i] = i < len ? s1[i] : s2[i - len];
+
+	return
+(new_str);
 }
