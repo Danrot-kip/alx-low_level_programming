070763c023900f39715711a6db6f8fcc825ffad3
Kipyegon Rotich <danyego100@gmail.com>
2022-08-06 03:43:57 -0700

    functions and pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,6 +4,12 @@
 
 #include <string.h>
 #include <stddef.h>
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char c);
