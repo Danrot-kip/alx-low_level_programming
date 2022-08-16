63832707729928b375020eb382d111ca9bfa1855
Kipyegon Rotich <danyego100@gmail.com>
2022-08-16 13:18:56 -0700

    Linked lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,6 +4,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+/**
+ * struct listint_s - list integer
+ */
+
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
