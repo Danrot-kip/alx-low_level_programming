7840047af51215443dbf27049b0f01505cd541ae
Kipyegon Rotich <danyego100@gmail.com>
2022-10-06 03:03:23 -0700

    libdynamic.so

---
+++ 0x18-dynamic_libraries/lists/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * _abs - _abs prints out the absolute value
+ *@n: the unteger we are checking
+ * Return: return the absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
